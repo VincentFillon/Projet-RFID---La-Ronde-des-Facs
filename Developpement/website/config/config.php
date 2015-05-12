269b4c7b5423c4b80503a15200fc1db0ed6a3c66
Vincent FILLON <v.fillon.pro@gmail.com>
2015-05-12 20:36:17 +0200

    YES MAN
    Connection SQL SERVER BRO !

--- Developpement/website/config/config.php
+++ Developpement/website/config/config.php
@@ -15,10 +15,26 @@ $salt = sha1('pr0n0S31');
 // Connect to server and select database.
 try
 {
-    $dbh = new PDO('mysql:host=' . $hote . ';dbname=' . $bdd ,$login_bdd , $mdp_bdd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    $dbh = new PDO('mysql:host=' . $hote . ';dbname=' . $bdd ,$login_bdd , $mdp_bdd,
+        array(
+            PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+        )
+    );
 }
 
 catch(Exception $e)
 {
     die('Erreur :'.$e->getMessage());
+}
+
+$server = '(local)';
+$connexionInfo = array("Database"=>"FacPrincipal");
+$link = sqlsrv_connect($server,$connexionInfo);
+if( $link ) {
+    echo "Connexion établie.<br />";
+}else{
+    echo "La connexion n'a pu être établie.<br />";
+    die( print_r( sqlsrv_errors(), true));
 }
