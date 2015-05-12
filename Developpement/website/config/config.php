47025798c50cd63a7508a139040ab60e0b337998
Vincent FILLON <v.fillon.pro@gmail.com>
2015-05-13 00:00:59 +0200

    Retrait config mySQL
    et ouais bro

--- Developpement/website/config/config.php
+++ Developpement/website/config/config.php
@@ -6,29 +6,6 @@
  * Time: 10:46
  */
 
-$hote = '37.187.120.205';
-$login_bdd = 'rondedesfacs';
-$mdp_bdd = 'rdf2016';
-$bdd = 'FacPrincipal';
-$salt = sha1('pr0n0S31');
-
-// Connect to server and select database.
-try
-{
-    $dbh = new PDO('mysql:host=' . $hote . ';dbname=' . $bdd ,$login_bdd , $mdp_bdd,
-        array(
-            PDO::MYSQL_ATTR_LOCAL_INFILE => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-        )
-    );
-}
-
-catch(Exception $e)
-{
-    die('Erreur :'.$e->getMessage());
-}
-
 $server = '(local)';
 $connexionInfo = array("Database"=>"FacPrincipal", 'ReturnDatesAsStrings'=>true);
 $link = sqlsrv_connect($server,$connexionInfo);
